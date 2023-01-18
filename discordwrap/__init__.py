e932260a87661841e428074c9368bf99c193665c
Josh Brown <josh@mjbrowns.com>
2023-01-18 11:46:03 -0500

    feat: add black

--- discordwrap/__init__.py
+++ discordwrap/__init__.py
@@ -1,2 +1,3 @@
 from .helloworld import hello
-__version__ = '0.2.0'
+
+__version__ = "0.2.0"
--- tests/test_discord_wrap.py
+++ tests/test_discord_wrap.py
@@ -2,7 +2,8 @@ from discordwrap import __version__, hello
 
 
 def test_version():
-    assert __version__ == '0.2.0'
+    assert __version__ == "0.2.0"
+
 
 def test_hello():
     assert hello() == "world"
