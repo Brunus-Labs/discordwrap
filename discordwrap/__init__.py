bd25cabf010cce2cec1fdcde4661fd70cf681a90
Josh Brown <josh@mjbrowns.com>
2023-01-18 11:44:52 -0500

    feat: add test function

--- discordwrap/__init__.py
+++ discordwrap/__init__.py
@@ -1,1 +1,2 @@
+from .helloworld import hello
 __version__ = '0.2.0'
--- tests/test_discord_wrap.py
+++ tests/test_discord_wrap.py
@@ -1,5 +1,8 @@
-from discordwrap import __version__
+from discordwrap import __version__, hello
 
 
 def test_version():
     assert __version__ == '0.2.0'
+
+def test_hello():
+    assert hello() == "world"
