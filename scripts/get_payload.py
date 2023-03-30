0b1e00e2298b8aa2bdb04042a2588425d4beb099
josh <josh@mjbrowns.com>
2023-03-29 23:27:51 -0400

    feat: abstract out rate limit logic

--- scripts/get_payload.py
+++ scripts/get_payload.py
@@ -8,6 +8,14 @@ import discordwrap
 
 dotenv.load_dotenv()
 
+
+def surround(color: str, *args) -> str:
+    msg = ""
+    for arg in args:
+        msg += str(arg)
+    return color + msg + "\033[0m"
+
+
 if __name__ == "__main__":
     TOKEN = os.getenv("DISC_TOKEN")
     if TOKEN == None:
@@ -15,9 +23,27 @@ if __name__ == "__main__":
         sys.exit()
 
     discordwrap.Auth.TOKEN = TOKEN
-    path = input("Request url: ")
-    method = input("Method: ")
+    path = input("Request Url: " + surround("\033[90m", "https://discord.com/api/v10"))
+    request_type = input("Request Method " + surround("\033[90m", "(POST)") + ": ")
+    method = input("Method Name: ").upper()
+    if method != "POST":
+        input(
+            "Make sure that the body json is in "
+            + surround("\u001b[34m", "body.json")
+            + "\n"
+        )
+
+    while len(path) == 0:
+        print("Requires request path")
+        path = input(
+            "Request url: " + surround("\033[90m", "https://discord.com/api/v10")
+        )
+    while len(method) == 0:
+        print("Requires method name")
+        method = input("Method: ")
+
+    path = path.upper()
     res = discordwrap.core.post(path)
-    print(res[1])
+
     with open(f"./tests/responses/{method}.json", "w") as file:
-        file.write(json.dumps(res[0], indent=2))
+        file.write(json.dumps(res, indent=2))
